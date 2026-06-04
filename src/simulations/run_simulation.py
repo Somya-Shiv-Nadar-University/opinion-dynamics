392137f014c2d981e183d93a373c344250dd538f
somya <somya.singh@snu.edu.in>
2026-06-04 19:24:21 +0530

    Added non-homogeneous bot figure again

--- src/simulations/run_simulation.py
+++ src/simulations/run_simulation.py
@@ -1,82 +1,18 @@
-from src.models.od_model import *
-from src.plotting.network_plot import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-# ==================================================
-# Parameters
-# ==================================================
-
-N = 10
-L = 10
-M = 3
-T = 10
-Time = 200
-
-# ==================================================
-# Generate model ingredients
-# ==================================================
-
-c1 = np.load("data/c1_N10_M5.npy")
-c2 = np.load("data/c2_N10_M5.npy")
-memory_sets = generate_memory_sets(
-    N=N,
-    L=L,
-    M=M
+# load data
+all_means = np.load(
+    "data/all_means_N10_M5_nonhomo_bot.npy"
 )
 
-# ==================================================
-# Draw network
-# ==================================================
-A = np.load("data/A_N10_M5.npy")
-# ==================================================
-# Simulate empirical sums for all agents
-# ==================================================
+# time axis
+time = np.arange(3, all_means.shape[0] + 3)
 
-all_means = np.zeros((Time,N))
-
-rng = np.random.default_rng(42)
-
-for horizon in range(3, Time + 3):
-
-    monte_carlo_vals = []
-
-    for sim in range(5000):
-
-        X_history, Z_history = simulate_trajectory(
-            N=N,
-            horizon=horizon,
-            T=T,
-            L=L,
-            A=A,
-            c1=c1,
-            c2=c2,
-            memory_sets=memory_sets,
-            rng=rng,
-        )
-
-        ergodic_vals = np.mean(Z_history, axis=0)
-
-        monte_carlo_vals.append(
-            ergodic_vals
-        )
-
-    all_means[horizon - 3] = np.mean(
-    monte_carlo_vals,
-    axis=0
-)
-#save the empirical sum data
-np.save(
-    "data/all_means_N10_M3.npy",all_means
-)
-
-# ==================================================
-# Plotting
-# ==================================================
-
-time = np.arange(3, Time + 3)
-
+# plot
 plt.figure(figsize=(10, 4.5))
 
-for node in range(N):
+for node in range(all_means.shape[1]):
 
     plt.plot(
         time,
@@ -85,20 +21,22 @@ for node in range(N):
         label=f"Agent {node+1}"
     )
 
-plt.legend(
-    bbox_to_anchor=(1.02, 1),
-    loc="upper left",
-    borderaxespad=0.
-)
-plt.tight_layout()
-
 plt.xlabel("Time")
 plt.ylabel("Average empirical sum")
 
+plt.title(
+    r"Non-homogeneous model with bot: $\beta=0.2,\ \eta_B=0.8$"
+)
+
+plt.legend(
+    bbox_to_anchor=(1.02, 1),
+    loc="upper left"
+)
+
+plt.tight_layout()
 plt.savefig(
-    "figures/all_means_N10_M3.png",
+    "figures/all_means_N10_M5_nonhomo_bot.png",
     dpi=300,
     bbox_inches="tight"
 )
-
 plt.show()
